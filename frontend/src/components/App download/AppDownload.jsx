b67b7d7ac419de8e3c062d95f92e30c1dae4f645
nimrat6860 <163638200+nimrat6860@users.noreply.github.com>
2025-08-23 22:35:16 +0530

    Update AppDownload.jsx

--- frontend/src/components/App download/AppDownload.jsx
+++ frontend/src/components/App download/AppDownload.jsx
@@ -1,16 +1,1 @@
-import React from 'react'
-import './AppDownload.css' 
-import { assets } from '../../assets/assets'
-const AppDownload = () => {
-  return (
-    <div className='appdownload' id ='appdownload'>
-        <p>For better experience Download <br/> Foodingo App</p>
-        <div className="appdownloadplatforms"> 
-            <img src={assets.play_store} alt="" />
-            <img src={assets.app_store} alt="" />
-        </div>
-    </div>
-  )
-}
 
-export default AppDownload
