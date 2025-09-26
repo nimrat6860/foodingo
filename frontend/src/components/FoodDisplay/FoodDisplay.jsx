610976f9b00db17b4fb0acecd34db13c47162ba8
nimrat6860 <163638200+nimrat6860@users.noreply.github.com>
2025-09-26 15:51:31 +0530

    Update FoodDisplay.jsx

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -3,7 +3,18 @@ import "./FoodDisplay.css";
 import { StoreContext } from "../../Context/Context";
 import FoodItem from "../FoodItem/FoodItem";
 const FoodDisplay = ({ category }) => {
-  const { food_list } = useContext(StoreContext);
+  const { food_list,loading } = useContext(StoreContext);
+  if (loading) {
+  return (
+    <div className="loader-container">
+      <div className="loader"></div>
+      <div className="loader-text">
+        Fetching fresh <span>dishes 🍲</span> ...
+      </div>
+    </div>
+  );
+}
+
   return (
     <div className="food-display" id="food-display">
       <h2>Top Dishes near you</h2>
